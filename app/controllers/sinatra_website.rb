4ea632e26279f8a021dc6919209bb059397f636a
Colin Alexander <colintalex@gmail.com>
2020-09-01 14:36:02 -0600

    Implements HTTP Basic Auth for Admin access

--- app/controllers/sinatra_website.rb
+++ app/controllers/sinatra_website.rb
@@ -1,4 +1,20 @@
 class SinatraWebsite < Sinatra::Base
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not Authorized\n"
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+    end
+  end
+
+  use Rack::Auth::Basic do |username, password|
+    username == 'admin' and password == 'admin'
+  end
 
   get '/' do
     erb :welcome, :layout => :template
@@ -8,5 +24,10 @@ class SinatraWebsite < Sinatra::Base
     @projects = Project.all
     @graphics = Graphic.all
     erb :work, :layout => :template
+  end
+
+  get '/admin' do
+    protected!
+    erb :dashboard, :layout => :template
   end
 end
