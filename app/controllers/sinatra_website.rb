26e3ffda689c8ec1bacc382a3487908ff9c6cc9d
Colin Alexander <colintalex@gmail.com>
2020-09-01 13:16:29 -0600

    Creates work view, spec and route

--- app/controllers/sinatra_website.rb
+++ app/controllers/sinatra_website.rb
@@ -3,4 +3,10 @@ class SinatraWebsite < Sinatra::Base
   get '/' do
     erb :welcome, :layout => :template
   end
+
+  get '/work' do
+    @projects = Project.all
+    @graphics = Graphic.all
+    erb :work, :layout => :template
+  end
 end
