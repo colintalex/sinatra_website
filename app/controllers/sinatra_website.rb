d032559d4337598f8cdaf82a52442e1631a89c08
Colin Alexander <colintalex@gmail.com>
2020-09-02 16:13:39 -0600

    Routes created for create and destroy. Adds MethodOverride for delete

--- app/controllers/sinatra_website.rb
+++ app/controllers/sinatra_website.rb
@@ -1,20 +1,9 @@
 class SinatraWebsite < Sinatra::Base
-  helpers do
-    def protected!
-      return if authorized?
-      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
-      halt 401, "Not Authorized\n"
-    end
+  use Rack::MethodOverride
 
-    def authorized?
-      @auth ||= Rack::Auth::Basic::Request.new(request.env)
-      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
-    end
-  end
-
-  use Rack::Auth::Basic do |username, password|
-    username == 'admin' and password == 'admin'
-  end
+  # use Rack::Auth::Basic do |username, password|
+  #   username == 'admin' and password == 'admin'
+  # end
 
   get '/' do
     erb :welcome, :layout => :template
@@ -27,7 +16,33 @@ class SinatraWebsite < Sinatra::Base
   end
 
   get '/admin' do
-    protected!
+    # protected!
+    @projects = Project.all
+    @graphics = Graphic.all
     erb :dashboard, :layout => :template
+  end
+
+  get '/admin/projects/new' do
+    # protected!
+    erb :new_project, :layout => :template
+  end
+
+  post '/admin/projects' do
+    proj = Project.new(params)
+    if proj.save
+      redirect '/admin'
+    else
+      redirect '/admin/projects/new'
+    end
+  end
+
+  delete "/admin/projects/:id" do
+    proj = Project.find(params[:id])
+    proj.destroy
+    redirect '/admin'
+  end
+
+  not_found do
+    'This is nowhere to be found.'
   end
 end
