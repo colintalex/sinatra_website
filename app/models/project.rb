fc01a25779aa07139a2faaf44ad298a986ed35d6
Colin Alexander <colintalex@gmail.com>
2020-09-02 16:13:16 -0600

    Creates new project form and new project link on dashboard

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ActiveRecord::Base
-  
+  validates_presence_of :title, uniqueness: true
+  validates_presence_of :description
+  validates_presence_of :image
+  validates_presence_of :github_url
+
 end
