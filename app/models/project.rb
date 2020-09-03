9f06c4f13c500230f619ce7579b99dd50ebf4899
Colin Alexander <colintalex@gmail.com>
2020-09-03 01:34:07 -0600

    HTML changes, and removes image validation for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ActiveRecord::Base
   validates_presence_of :title, uniqueness: true
   validates_presence_of :description
-  validates_presence_of :image
   validates_presence_of :github_url
 
 end
